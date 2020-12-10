50f1046176e779e52eb8d59b173ef4fde28f5c8a
baromeg <baromeg@gmail.com>
2020-12-10 21:02:00 +0000

    Navbar and colours fixed

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -5,6 +5,7 @@ import { getUserId } from '../lib/auth'
 
 const Navbar = (props) => {
   const [user, updateUser] = useState({})
+  const [isActive, setisActive] = useState(false)
 
   const currentUserId = getUserId()
   // console.log(currentUserId)
@@ -23,31 +24,39 @@ const Navbar = (props) => {
       })
   }, [])
 
-  return <nav className="navbar is-link">
-    <div className="navbar-menu is-active">
+  return <nav className="navbar is-link" role='navigation' aria-label="main navigation">
+    <div className="navbar-brand">
+      <img className="image is-64x64" src="https://res.cloudinary.com/greenupload/image/upload/v1607631126/logo_gva9qr.jpg" alt="logo" />
+      <a role="button" className={`navbar-burger burger ${isActive ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false" data-target="navbarinfo" onClick={() => setisActive(!isActive)}>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarinfo" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
       <div className="navbar-start">
         <div className="navbar-item">
-          <div className="buttons">
+          <div className="buttons ">
             <Link className="button is-light" to="/">Home</Link>
-            <Link className="button is-grey-dark" to="/sites">List of Sites</Link>
+            <Link className="button is-primary has-text-weight-semibold" to="/sites">KNOW YOUR HERITAGE</Link>
           </div>
 
         </div>
 
       </div>
-
       <div className="navbar-end">
         <div className="navbar-item">
           <div className="buttons">
 
             {!localStorage.getItem('token') && <Link className="button is-light" to="/signup">Sign up</Link>}
             {!localStorage.getItem('token') &&
-              <Link className="button is-grey-dark" to="/login">Login</Link>}
+              <Link className="button is-primary has-text-weight-semibold" to="/login">Login</Link>}
 
 
             {localStorage.getItem('token') &&
-              <div className="mr-3"> <p> Welcome back <Link className="is-capitalized is-grey-dark" to={`/user/${currentUserId}`}>
-                <strong className="is-capitalized is-grey-dark"> {user.username} </strong></Link></p></div>
+              <div className="mr-3"> <p> Welcome back <Link className="is-capitalized" to={`/user/${currentUserId}`}>
+                <strong id="stronguser"> {user.username} </strong></Link></p></div>
             }
 
             {localStorage.getItem('token') && <button
@@ -55,12 +64,15 @@ const Navbar = (props) => {
               onClick={handleLogout}
             >
               Logout
-            </button>}
+          </button>}
           </div>
 
         </div>
       </div>
+
     </div>
+
+
   </nav>
 }
 
