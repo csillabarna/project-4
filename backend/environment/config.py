5ddaaa4ebdb6497c3f0219585cba39c4c8b75941
Csilla Barna <barnacsilla89@gmail.com>
2020-12-10 12:51:49 +0000

    deploy update

--- backend/environment/config.py
+++ backend/environment/config.py
@@ -1,9 +1,6 @@
 import os
-db_host = os.environ['POSTGRES_HOST']
-db_port = os.environ['POSTGRES_PORT']
-db_user = os.environ['POSTGRES_USER']
-db_pass = os.environ['POSTGRES_PASS']
 secret = os.environ['JWT_SECRET']
 Google_API = os.environ['Google_API']
 MapBox_API = os.environ['MapBox_API']
-db_URI = f'postgres://{db_host}:{db_port}/heritage_db?user={db_user}&password={db_pass}'
+db_URI = os.environ['DATABASE_URL']
+
